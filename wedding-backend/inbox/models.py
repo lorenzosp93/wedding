a1444095cc861cd8e3c7ad800aa58302dab710f6
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-07-04 11:22:41 +0200

    inbox models improvement

--- wedding-backend/inbox/models.py
+++ wedding-backend/inbox/models.py
@@ -4,10 +4,20 @@ from shared.models import (
     Serializable, Named, TimeStampable,
 )
 I18N = (
-    (1, 'en'),
-    (2, 'it'),
-    (3, 'es'),
+    (0, 'en'),
+    (1, 'it'),
+    (2, 'es'),
 )
+USER_TYPES = (
+    (0, 'family'),
+    (1, 'friend'),
+    (2, 'colleague')
+)
+
+class UserExtended(User):
+    language = models.IntegerField(choices=I18N)
+    type = models.IntegerField(choices=USER_TYPES)
+
 # Create your models here.
 class ContentString(Named):
     pass
@@ -15,16 +25,21 @@ class ContentString(Named):
 class TranslatedString(models.Model):
     language = models.IntegerField(choices=I18N)
     string = models.TextField()
+    content = models.ForeignKey(ContentString, on_delete=models.CASCADE)
 
 class Message(Serializable, Named):
-    content = models.ForeignKey(ContentString, on_delete=models.CASCADE)
+    content = models.ForeignKey(TranslatedString, on_delete=models.CASCADE)
+
+class UserMessage(Serializable):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    recipient = models.ForeignKey(UserExtended, on_delete=models.CASCADE)
 
 class Question(Serializable, Named):
-    content = models.ForeignKey(ContentString, on_delete=models.CASCADE)
+    content = models.ForeignKey(TranslatedString, on_delete=models.CASCADE)
     message = models.ForeignKey(Message, on_delete=models.CASCADE)
 
 class Option(Named):
-    content = models.ForeignKey(ContentString, on_delete=models.CASCADE)
+    content = models.ForeignKey(TranslatedString, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
 class Response(TimeStampable):
