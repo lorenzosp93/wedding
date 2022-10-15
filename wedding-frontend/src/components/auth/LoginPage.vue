e732408e4f27da93ee1488e0f23342bad2b99a63
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-10-15 15:13:44 +0200

    mistake

--- wedding-frontend/src/components/auth/LoginPage.vue
+++ wedding-frontend/src/components/auth/LoginPage.vue
@@ -5,7 +5,7 @@
         <label class="block mx-auto" for="email_input">{{ $t('auth.loginpage.emailAddress') }}</label>
         <input v-model="email" class="block bg-neutral dark:bg-darkNeutral border-2 border-white rounded-md mx-auto px-2 " id="email_input" type="email">
         <p v-if="!!error" class="text-alert font-bold mx-auto">There was an error sending you the access code: {{ error }}</p>
-        <button v-if="!loading" @click.prevent.once="handleLogin(email)" class="flex border-2 border-white mx-auto my-4 px-2 py-1 rounded-lg bg-neutral dark:bg-darkNeutral" type="submit">{{ $t('auth.loginpage.submit') }}</button>
+        <button v-if="!loading" @click.prevent="handleLogin(email)" class="flex border-2 border-white mx-auto my-4 px-2 py-1 rounded-lg bg-neutral dark:bg-darkNeutral" type="submit">{{ $t('auth.loginpage.submit') }}</button>
         <p v-else>Loading</p>
       </form>
   </div>
