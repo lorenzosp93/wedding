a2b50348975828cfadd042db99dddd2c12d9d027
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-11-16 17:03:36 +0100

    browser support

--- wedding-frontend/vite.config.js
+++ wedding-frontend/vite.config.js
@@ -6,54 +6,58 @@ import vue from '@vitejs/plugin-vue'
 import vueI18n from '@intlify/vite-plugin-vue-i18n'
 import viteCompression from 'vite-plugin-compression'
 import { VitePWA } from 'vite-plugin-pwa'
+import legacy from '@vitejs/plugin-legacy'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue(),
-        vueI18n({
-            include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
-        }),
-        viteCompression(),
-        VitePWA({
-            registerType: 'autoUpdate',
-            devOptions: {
-                enabled: true,
-            },
-            includeAssets: [
-                'favicon.ico',
-                'favicon-16x16.ico',
-                'favicon-32x32.ico',
-                'apple-touch-icon.png',
-                'masked-icon.svg'
-            ],
-            manifest: {
-              name: 'Priscilla & Lorenzo - wedding app',
-              short_name: 'PriscillaLorenzo',
-              description: "The web application to keep track of Priscilla and Lorenzo's wedding",
-              theme_color: '#D5B19B',
-              icons: [
-                {
-                  src: 'pwa-192x192.png',
-                  sizes: '192x192',
-                  type: 'image/png'
-                },
-                {
-                  src: 'pwa-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png'
-                }
-              ]
-            }
-          })
-
-    ],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL('./src', import.meta.url)),
-        },
-    },
-    server: {
-        port: 8080,
-    }
-  })
+  plugins: [
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    }),
+    vue(),
+    vueI18n({
+      include: resolve(dirname(fileURLToPath(import.meta.url)), './src/i18n/locales/**'),
+    }),
+    viteCompression(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+          enabled: true,
+      },
+      includeAssets: [
+          'favicon.ico',
+          'favicon-16x16.ico',
+          'favicon-32x32.ico',
+          'apple-touch-icon.png',
+          'masked-icon.svg'
+      ],
+      manifest: {
+        name: 'Priscilla & Lorenzo - wedding app',
+        short_name: 'PriscillaLorenzo',
+        description: "The web application to keep track of Priscilla and Lorenzo's wedding",
+        theme_color: '#D5B19B',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+      alias: {
+          "@": fileURLToPath(new URL('./src', import.meta.url)),
+      },
+  },
+  server: {
+      port: 8080,
+  }
+})
