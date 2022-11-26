7c850dcd1a53d141300cfb8eb2cd679cf4256990
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-11-26 16:25:08 +0100

    fix next on reload

--- wedding-frontend/src/stores/api.store.js
+++ wedding-frontend/src/stores/api.store.js
@@ -173,7 +173,7 @@ export const useGalleryStore = defineStore({
         error: null,
         gallery: JSON.parse(localStorage.getItem('gallery') ?? "[]"),
         galleryExpiry: Date.parse(localStorage.getItem('galleryExpiry') ?? new Date()),
-        next: null,
+        next: localStorage.getItem('galleryNext'),
     }),
     actions: {
         async getGalleryContent (force=null) {
@@ -191,6 +191,7 @@ export const useGalleryStore = defineStore({
                         if (!force) {
                             localStorage.setItem('gallery', JSON.stringify(this.gallery));
                             localStorage.setItem('galleryExpiry', JSON.stringify(this.galleryExpiry));
+                            localStorage.setItem('galleryNext', this.next);
                         }
                     }
                 ).catch(
