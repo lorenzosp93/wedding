bcf9bdf230501bbc723c35f86181f5f4e3e97211
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-11-28 17:15:45 +0100

    remove useless method in list view

--- wedding-frontend/src/components/shared/ListView.vue
+++ wedding-frontend/src/components/shared/ListView.vue
@@ -176,26 +176,19 @@ emits: ['submitResponse', 'deleteResponses'],
       this.viewDetail = true;
     },
     removeHtml (value) {
-    const div = document.createElement('div')
-    div.innerHTML = value
-    const text = div.textContent || div.innerText || ''
-    return text
+      const div = document.createElement('div')
+      div.innerHTML = value
+      const text = div.textContent || div.innerText || ''
+      return text
     },
     truncate (value, length) {
-    if (!value) return "";
-    value = value.toString();
-    if (value.length > length) {
-        return value.substring(0, length) + "...";
-    } else {
-        return value;
-    }
-    },
-    formattedDate (date) {
-    date = new Date(date);
-    const yyyy = date.getYear() + 1900;
-    const mm = date.getMonth() + 1;
-    const dd = date.getDate();
-    return `${yyyy}.${mm}.${dd}`
+      if (!value) return "";
+      value = value.toString();
+      if (value.length > length) {
+          return value.substring(0, length) + "...";
+      } else {
+          return value;
+      }
     },
     hideDetail () {
       this.viewDetail = false;
