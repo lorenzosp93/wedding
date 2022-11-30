4bfc9fac1059b279850bab1824e31b9d2769f2f5
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-11-30 19:24:52 +0100

    fix routing serviceworker

--- wedding-frontend/vite.config.js
+++ wedding-frontend/vite.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
       registerType: 'autoUpdate',
       workbox:{
         importScripts: ['/push-sw.js'],
+        navigateFallbackDenylist: [/\/api\//],
       },
       devOptions: {
         enabled: process.env.DEV,
