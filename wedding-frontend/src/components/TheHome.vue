dfcba5e32cf01b60a5cd088ddbfbd25741027bb9
Lorenzo Spinelli <lorenzosp93@gmail.com>
2023-12-08 18:40:20 +0100

    Refactor login text and invitation link + translations

--- wedding-frontend/src/components/TheHome.vue
+++ wedding-frontend/src/components/TheHome.vue
@@ -32,31 +32,28 @@
         class="my-10 mx-auto"
       />
       <div
-        v-if="authStore.isAuthenticated"
         id="invite-link"
-        class="flex w-fit mx-auto my-8 bg-accent text-primary px-2 py-1 rounded-md shadow-md"
+        class="flex w-fit mx-auto my-8 bg-accent text-primary px-2 rounded-md shadow-md"
       >
-        <RouterLink :to="{ name: 'invitation' }" class="flex">
+        <RouterLink :to="{ name: 'invitation' }" class="flex my-1">
           <EnvelopeIcon class="w-5 h-5 mr-1 my-auto" />
-          <p class="mx-1">
+          <p class="mx-1 my-auto">
             {{ $t("thehome.openYourInvitation") }}
           </p>
         </RouterLink>
-      </div>
-      <div
-        id="login"
-        v-else
-        class="flex w-fit mx-auto my-8 bg-accent text-primary px-2 py-1 rounded-md shadow-md"
-      >
-        <RouterLink :to="{ name: 'login' }" class="flex">
+        <RouterLink
+          v-if="!authStore.isAuthenticated"
+          :to="{ name: 'login' }"
+          class="flex border-l-2 pl-2 ml-2"
+        >
           <UserIcon class="w-5 h-5 mr-1 my-auto" />
-          <p class="mx-1">
+          <p class="mx-1 my-auto">
             {{ $t("shared.thenavbar.login") }}
           </p>
         </RouterLink>
       </div>
     </div>
-    <OnboardingTour v-if="authStore.profile?.id" />
+    <OnboardingTour v-if="authStore.isAuthenticated" />
     <TheFooter />
   </div>
 </template>
